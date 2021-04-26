5352215e7e1ce0f29fe0004b7c3e935daabaf36f
Bogong Yang <cmdroot9908@gmail.com>
2021-04-26 00:42:40 -0400

    more model changes

--- backend/blueprints/api.py
+++ backend/blueprints/api.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, jsonify, Blueprint, current_app
 
 from backend.extensions import db
-from backend.models import User
+from backend.models import User, Payment, Transaction
 # create blueprint for api endpoints
 api_bp = Blueprint('api', __name__)
 
@@ -53,3 +53,89 @@ def get_all_users():
     }
 
     return jsonify(res), 200
+
+
+# use for test only
+# payments should be created with transaction in a batch, not individually
+@api_bp.route('/api/add-payment', methods=['POST'])
+def add_payment():
+    req = flask.request.get_json()
+    print(req)
+
+    username = req['username']
+    amount = req['amount']
+
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        res = {
+            'msg': f'User with name {username} does not exist'
+        }
+
+        return jsonify(res), 400
+    
+    payment = Payment(user.id, 0, amount=amount)
+
+    db.session.add(payment)
+    db.session.commit()
+
+    res = {
+        'msg': 'ok'
+    }
+
+    return jsonify(res), 200
+    pass
+
+@api_bp.route('/api/get-all-payments', methods=['GET'])
+def get_all_payments():
+    # req = flask.request.get_json()
+
+    payments = list()
+    for payment in Payment.query.all():
+        payments.append(payment.to_dict())
+    
+    res = {
+        'payments': payments
+    }
+
+    return jsonify(payments), 200
+
+
+@api_bp.route('/api/add-transaction', methods=['POST'])
+def add_transaction():
+
+    req = flask.request.get_json()
+
+    transaction = Transaction()
+    db.session.add(transaction)
+
+    for payment in req['payments']:
+        username = payment['username']
+        amount = payment['amount']
+
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None:
+            res = {
+                'msg': f'User with name {username} does not exist'
+            }
+
+            return jsonify(res), 400
+        
+        payment = Payment(user_id=user.id, transaction_id=transaction_id, amount=amount)
+        db.session.add(payment)
+    
+    db.session.commit()
+
+    res = {
+        'msg': 'ok'
+    }
+
+    return jsonify(res), 200
+
+@api_bp.route('/api/get-all-transactions', method=['GET', 'POST'])
+def get_all_transactions():
+    transactions = list()
+
+    for transaction in Transaction.query.all():
+        
